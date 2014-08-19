dbe4259e1d07259fa86abddc14c1686112dc8f19
Chris Cowell-Shah <ccs@puppetlabs.com>
2014-08-19 09:19:54 -0700

    refactor for streamlining

--- delete_all_groups.rb
+++ delete_all_groups.rb
@@ -8,26 +8,20 @@ if ARGV.length != 1
 end
 
 BASE_URI = ARGV[0]
-
 OPTIONS = {:verify => false}
 
 begin
   response = HTTParty.get("https://#{BASE_URI}:4431/node_groups/classifier-api/v1/groups", OPTIONS)
 rescue SocketError => se
   puts "ERROR when contacting instance '#{BASE_URI}' via API. Maybe that's a bad hostname?"
-  puts
   exit 1
 end
 
 delete_count = 0
 response.each do |group|
-  group_id = group['id']
-  group_name = group['name']
-
-  next if group_name == 'default'
-
-  puts "deleting ID #{group_id} with name #{group_name}"
-  HTTParty.delete("https://#{BASE_URI}:4431/node_groups/classifier-api/v1/groups/#{group_id}", OPTIONS)
+  next if group['name'] == 'default'
+  puts "deleting ID #{group['id']} with name #{group['name']}"
+  HTTParty.delete("https://#{BASE_URI}:4431/node_groups/classifier-api/v1/groups/#{group['id']}", OPTIONS)
   delete_count += 1
 end
-puts "Deleted #{delete-count} groups."
+puts "deleted #{delete-count} groups"
