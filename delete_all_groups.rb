b0b1f3328d0ac099a5cc37cd4cf7ce417ef57439
Chris Cowell-Shah <ccs@puppetlabs.com>
2014-08-19 09:12:43 -0700

    added error handling for bad host names

--- delete_all_groups.rb
+++ delete_all_groups.rb
@@ -1,14 +1,25 @@
+# Delete all node groups from a Puppet test instance, using the API. This cannot be undone!
+
 require 'HTTParty'
 
 if ARGV.length != 1
   puts 'ERROR: YOU MUST PASS IN THE PUPPET MASTER VCLOUD MACHINE NAME AS THE ONLY ARG'
-  exit
+  exit 1
 end
 
 BASE_URI = ARGV[0]
 
 OPTIONS = {:verify => false}
-response = HTTParty.get("https://#{BASE_URI}:4431/node_groups/classifier-api/v1/groups", OPTIONS)
+
+begin
+  response = HTTParty.get("https://#{BASE_URI}:4431/node_groups/classifier-api/v1/groups", OPTIONS)
+rescue SocketError => se
+  puts "ERROR when contacting instance '#{BASE_URI}' via API. Maybe that's a bad hostname?"
+  puts
+  exit 1
+end
+
+delete_count = 0
 response.each do |group|
   group_id = group['id']
   group_name = group['name']
@@ -17,5 +28,6 @@ response.each do |group|
 
   puts "deleting ID #{group_id} with name #{group_name}"
   HTTParty.delete("https://#{BASE_URI}:4431/node_groups/classifier-api/v1/groups/#{group_id}", OPTIONS)
+  delete_count += 1
 end
-
+puts "Deleted #{delete-count} groups."
